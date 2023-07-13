a6835ec88da81593198a663a84b2ec5b13e4b454
Jared Garcia <jaredgarcia@MacBook-Pro-de-Jared.local>
2023-07-13 09:53:00 -0600

    first commit

---
+++ src/data/monedas.js
@@ -0,0 +1,22 @@
+export const monedas = [
+  {
+    id: "HNL",
+    nombre: "Lempira",
+  },
+  {
+    id: "USD",
+    nombre: "Dolar de Estados Unidos",
+  },
+  {
+    id: "MXN",
+    nombre: "Peso Mexicano",
+  },
+  {
+    id: "EUR",
+    nombre: "Euro",
+  },
+  {
+    id: "GBP",
+    nombre: "Libra Esterlina",
+  },
+];
